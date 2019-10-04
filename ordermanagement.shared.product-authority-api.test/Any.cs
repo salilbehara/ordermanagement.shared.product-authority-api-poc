deb6caed2146ec15918c4968a6e236c22bf2e151
Kevin Markeles <kmarkeles@ebsco.com>
2019-10-04 13:30:10 -0500

    add command and query unit tests

---
+++ ordermanagement.shared.product-authority-api.test/Any.cs
@@ -0,0 +1,95 @@
+﻿using ordermanagement.shared.product_authority_infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ordermanagement.shared.product_authority_api.Application.Extensions;
+
+namespace ordermanagement.shared.product_authority_api.test
+{
+    public static class Any
+    {
+        private static readonly Random _random = new Random();
+
+        public static bool Bool() => _random.Next(0, 2) == 1;
+
+        public static decimal Decimal() => _random.Next(0, int.MaxValue);
+
+        public static long Long() => _random.Next(0, int.MaxValue);
+
+        public static string String(int length = 10)
+        {
+            var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            var stringBuilder = new StringBuilder();
+
+            for (var i = 0; i < length; i++)
+            {
+                stringBuilder.Append(chars[_random.Next(chars.Length)]);
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        public static int Int(int max = 1000) => _random.Next(max);
+        public static short Short(int max = 1000) => (short)_random.Next(max);
+
+        public static byte[] ByteArray(int length = 100)
+        {
+            var byteArray = new byte[length];
+
+            _random.NextBytes(byteArray);
+
+            return byteArray;
+        }
+
+        public static string[] StringArray(int length = 10)
+        {
+            var stringArray = new string[length];
+
+            for (var i = 0; i < length; i++)
+            {
+                stringArray.SetValue(Any.String(), i);
+            }
+
+            return stringArray;
+        }
+
+        public static ProductEntity ProductEntity(string productKey, DateTime effectiveDate, DateTime? expirationDate = null)
+        {
+            var productStatus = Any.ProductStatusEntity();
+            var productType = Any.ProductTypeEntity();
+            return new ProductEntity
+            {
+                ProductId = productKey.DecodeKeyToId(),
+                ProductKey = productKey,
+                AddedBy = Any.String(),
+                AddedOnUtc = DateTime.Now,
+                EffectiveStartDate = effectiveDate,
+                EffectiveEndDate = expirationDate ?? new DateTime(9999, 12, 31),
+                LegacyIdSpid = Any.Int(),
+                OnlineIssn = Any.String(8),
+                PrintIssn = Any.String(8),
+                ProductDisplayName = Any.String(),
+                ProductName = Any.String(),
+                PublisherId = Any.Long(),
+                UpdatedBy = Any.String(),
+                UpdatedOnUtc = DateTime.Now,
+                ProductStatus = productStatus,
+                ProductStatusCode = productStatus.ProductStatusCode,
+                ProductType = productType,
+                ProductTypeCode = productType.ProductTypeCode
+            };
+        }
+
+        public static ProductStatusEntity ProductStatusEntity() => new ProductStatusEntity
+        {
+            ProductStatusCode = Any.String(4),
+            ProductStatusName = Any.String()
+        };
+
+        public static ProductTypeEntity ProductTypeEntity() => new ProductTypeEntity
+        {
+            ProductTypeCode = Any.String(4),
+            ProductTypeName = Any.String()
+        };
+    }
+}
---
+++ ordermanagement.shared.product-authority-api.test/Queries/Products/GetProductBasedOnEffectiveStartDateQuery_should.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ordermanagement.shared.product_authority_api.Application.Commands.Products;
+using ordermanagement.shared.product_authority_api.Application.Queries.Products;
+using ordermanagement.shared.product_authority_infrastructure;
+using ordermanagement.shared.product_authority_infrastructure.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ordermanagement.shared.product_authority_api.test.Queries.Products
+{
+    public class GetProductBasedOnEffectiveStartDateQuery_should
+    {
+        private readonly ProductAuthorityDatabaseContext _context;
+        private List<ProductEntity> _products;
+        private GetProductBasedOnEffectiveStartDateQueryHandler _handler;
+        
+
+        public GetProductBasedOnEffectiveStartDateQuery_should()
+        {
+            _products = new List<ProductEntity>(new[]
+            {
+                Any.ProductEntity("key",new DateTime(2019, 01, 01),new DateTime(2019, 02, 01)),
+                Any.ProductEntity("key",new DateTime(2019, 02, 01))
+            });
+
+            _context = GetContextWithData();
+            _handler = new GetProductBasedOnEffectiveStartDateQueryHandler(_context);
+
+
+        }
+
+        [Fact]
+        public async void Get_Expired_Product_By_Effective_Date()
+        {
+            var effectiveDate = new DateTime(2019, 01, 15);
+            var query = new GetProductBasedOnEffectiveStartDateQuery("key", effectiveDate);
+            var response = await _handler.Execute(query);
+
+            Assert.Equal(_products.First().ProductName, response.ProductName);
+        }
+
+        private ProductAuthorityDatabaseContext GetContextWithData()
+        {
+            var options = new DbContextOptionsBuilder<ProductAuthorityDatabaseContext>()
+                              .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                              .Options;
+                        
+            var context = new ProductAuthorityDatabaseContext(options);
+
+            context.ProductStatuses.AddRange(new[]
+            {
+                new ProductStatusEntity {ProductStatusCode = "A", ProductStatusName = "StatusA"}
+            });
+
+            context.ProductTypes.AddRange(_products.Select(x => x.ProductType));
+            context.ProductStatuses.AddRange(_products.Select(x => x.ProductStatus));
+            context.Products.AddRange(_products);
+            
+            context.SaveChanges();
+            return context;
+        }
+    }
+}
